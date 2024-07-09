89ed71fcb0693a0e1ff9f637f324c7866ab5f1f5
freetimezp <kraftukrnet@ukr.net>
2024-07-09 22:16:53 +0300

    10. js-portfolio-101-beauty. Contact page.

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,20 +1,125 @@
 "use client";
-import React from 'react';
+import React, { useContext } from 'react';
+import Image from 'next/image';
 import { motion } from 'framer-motion';
 
+import { CursorContext } from '@/components/CursorContext';
+import Form from '@/components/Form';
+
 const Contact = () => {
+    const { mouseEnterHandler, mouseLeaveHandler } = useContext(CursorContext);
+
     return (
         <motion.section
             initial={{ opacity: 0 }}
-            animate={{
-                opacity: 1,
-                transition: {
-                    delay: 2,
-                }
-            }}
+            animate={{ opacity: 1, transition: { delay: 2 } }}
             className='min-h-screen flex items-center overflow-x-hidden'
         >
-            Contact page
+            <div className="container mx-auto pt-48 pb-12 xl:pt-32 xl:pb-0">
+                <div className="flex flex-col gap-12 xl:flex-row h-full">
+                    {/* text */}
+                    <motion.div
+                        className='flex-1 flex flex-col justify-center'
+                        onMouseEnter={mouseEnterHandler}
+                        onMouseLeave={mouseLeaveHandler}
+                        initial={{
+                            opacity: 0,
+                            x: -60
+                        }}
+                        animate={{
+                            opacity: 1,
+                            x: 0,
+                            transition: {
+                                delay: 2,
+                                duration: 1,
+                                ease: 'easeInOut'
+                            }
+                        }}
+                    >
+                        <h3 className='h3 mb-8 text-center xl:text-left'>
+                            Contact Info
+                        </h3>
+
+                        {/* items */}
+                        <div className='flex flex-col items-center xl:items-start gap-12'>
+                            <div className='flex items-start gap-4'>
+                                <div className='relative w-[36px] h-[36px]'>
+                                    <Image src="/assets/contact/pin.svg" fill alt="pin" />
+                                </div>
+
+                                <div className='pt-1 flex-1'>
+                                    <h4 className='h4 mb-2'>
+                                        Address
+                                    </h4>
+                                    <p className='leading-relaxed'>
+                                        123/45 My Street, Kyiv City
+                                        <br />
+                                        Ukraine, UA 69000
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div className='flex items-start gap-4'>
+                                <div className='relative w-[36px] h-[36px]'>
+                                    <Image src="/assets/contact/phone.svg" fill alt="pin" />
+                                </div>
+
+                                <div className='pt-1 flex-1'>
+                                    <h4 className='h4 mb-2'>
+                                        Phone number
+                                    </h4>
+                                    <div className='pt-1 flex-1'>
+                                        <p>Phone: +12 345 67890</p>
+                                        <p>Fax: +12 345 67890</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className='flex items-start gap-4'>
+                                <div className='relative w-[36px] h-[36px]'>
+                                    <Image src="/assets/contact/email.svg" fill alt="pin" />
+                                </div>
+
+                                <div className='pt-1 flex-1'>
+                                    <h4 className='h4 mb-2'>
+                                        Email address
+                                    </h4>
+                                    <div className='flex flex-col gap-1'>
+                                        <p>email@email.com</p>
+                                        <p>info@email.com</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </motion.div>
+
+                    {/* form */}
+                    <motion.div
+                        className='flex-1'
+                        onMouseEnter={mouseEnterHandler}
+                        onMouseLeave={mouseLeaveHandler}
+                        initial={{
+                            opacity: 0,
+                            x: 60
+                        }}
+                        animate={{
+                            opacity: 1,
+                            x: 0,
+                            transition: {
+                                delay: 2.4,
+                                duration: 1,
+                                ease: 'easeInOut'
+                            }
+                        }}
+                    >
+                        <div className='bg-[#f0cfbc] w-full max-w-[580px] gap-4 p-10 mx-auto
+                            xl:mx-0'>
+                            <h3 className='h3 mb-8 text-center'>Get in Touch</h3>
+                            <Form />
+                        </div>
+                    </motion.div>
+                </div>
+            </div>
         </motion.section>
     );
 }
